1a1962b5ba7d587d4262ec02e2d5d87a05d2db08
iicefy <chawit_rittiya@hotmail.com>
2025-05-03 15:42:03 +0700

    feat: add overflow-y-auto to ProductList container

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="bg-accent h-screen relative">
       <Header />
       <div className=" w-full grid grid-cols-[1fr_600px] h-full pt-16">
-        <div className=" h-full w-full">
+        <div className=" h-full w-full overflow-y-auto">
           <ProductList />
         </div>
         <div className="h-full bg-white">
