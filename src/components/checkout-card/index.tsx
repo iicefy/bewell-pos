4525dac882b52e02687bb67c1114ab8a7935da93
iicefy <chawit_rittiya@hotmail.com>
2025-05-03 15:42:45 +0700

    feat: enhance CheckoutCard component layout and add empty cart and checkout type files

--- src/components/checkout-card/index.tsx
+++ src/components/checkout-card/index.tsx
@@ -1,7 +1,12 @@
 // type CheckoutCardProps = {};
 
 const CheckoutCard = () => {
-  return <div></div>;
+  return (
+    <div className="flex gap-4">
+      <div className=""></div>
+      <div className="flex flex-col gap-2"></div>
+    </div>
+  );
 };
 
 export default CheckoutCard;
