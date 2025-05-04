7218193874711032f47bf4902fd9432cf678ef14
iicefy <chawit_rittiya@hotmail.com>
2025-05-04 21:23:34 +0700

    fix: ensure proper rendering of CheckoutCard components and add minimum height to CheckoutList

--- src/components/checkout-list/index.tsx
+++ src/components/checkout-list/index.tsx
@@ -6,20 +6,24 @@ const CheckoutList = () => {
   const { cart } = useCheckoutContext();
   return (
     <div className="relative h-full">
-      <div className="flex flex-col gap-4 bg-white h-full w-full border p-4 rounded-xl overflow-y-auto top-0 right-0 left-0 bottom-0 xl:absolute">
-        {cart?.map((item, i) => {
-          return (
-            <Fragment>
-              {item.amount ? (
-                <CheckoutCard data={item} key={item.productId} />
-              ) : null}
+      <div className="flex flex-col gap-4 bg-white min-h-40 h-full w-full border p-4 rounded-xl overflow-y-auto top-0 right-0 left-0 bottom-0 xl:absolute">
+        {cart.length ? (
+          cart?.map((item, i) => {
+            return (
+              <Fragment>
+                {item.amount ? (
+                  <CheckoutCard data={item} key={item.productId} />
+                ) : null}
 
-              {item.amountSendAfter ? (
-                <CheckoutCard data={item} key={i} isSendAfter />
-              ) : null}
-            </Fragment>
-          );
-        })}
+                {item.amountSendAfter ? (
+                  <CheckoutCard data={item} key={i} isSendAfter />
+                ) : null}
+              </Fragment>
+            );
+          })
+        ) : (
+          <span className="text-gray-500">No products added to the cart.</span>
+        )}
       </div>
     </div>
   );
