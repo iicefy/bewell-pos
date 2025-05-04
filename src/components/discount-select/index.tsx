3e117707b9e2e8b973dca9982dd28c9329d0c204
iicefy <chawit_rittiya@hotmail.com>
2025-05-04 15:38:40 +0700

    feat: add DiscountSelect component for selecting discount types

---
+++ src/components/discount-select/index.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { CheckIcon, ChevronDownIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+} from "../ui/command";
+type DiscountSelectProps = {
+  value: string;
+  onChange: (value: string) => void;
+};
+
+const DiscountSelect = ({ value, onChange }: DiscountSelectProps) => {
+  const [open, setOpen] = useState(false);
+  const discountList = [
+    { label: "Baht", value: "fixed" },
+    { label: "Percent", value: "percent" },
+  ];
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="bg-background hover:bg-background border-input justify-between px-3 font-normal outline-offset-0 outline-none focus-visible:outline-[3px]"
+        >
+          <span className={cn("truncate", !value && "text-muted-foreground")}>
+            {value
+              ? discountList.find((discount) => discount.value === value)?.label
+              : "Select discount type"}
+          </span>
+          <ChevronDownIcon
+            size={16}
+            className="text-muted-foreground/80 shrink-0"
+            aria-hidden="true"
+          />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        className="border-input w-full min-w-[var(--radix-popper-anchor-width)] p-0"
+        align="start"
+      >
+        <Command>
+          <CommandList>
+            <CommandEmpty>No framework found.</CommandEmpty>
+            <CommandGroup>
+              {discountList.map((discount) => (
+                <CommandItem
+                  key={discount.value}
+                  value={discount.value}
+                  onSelect={(currentValue) => {
+                    onChange(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                  }}
+                >
+                  {discount.label}
+                  {value === discount.value && (
+                    <CheckIcon size={16} className="ml-auto" />
+                  )}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default DiscountSelect;
